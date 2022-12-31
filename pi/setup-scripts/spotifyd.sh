717ce75b3c71b41009270c6c791965fd19a67151
Alistair Pattison <alistairpattison@gmail.com>
2022-12-31 05:20:18 +0000

    fixed spotifyd setup script

--- pi/setup-scripts/spotifyd.sh
+++ pi/setup-scripts/spotifyd.sh
@@ -2,7 +2,6 @@
 # SPOTIFYD SETUP
 # --------------
 
-CONFIG_FILE=~/.config/spotifyd/spotifyd.conf
 
 sudo -v # validate as sudo
 
@@ -15,20 +14,36 @@ read -p 'Speaker name: ' SPEAKER_NAME
 
 echo "Installing spotifyd..."
 wget "https://github.com/Spotifyd/spotifyd/releases/download/v0.3.4/spotifyd-linux-armv6-slim.tar.gz" && # download
-    tar xzf spotifyd-*.tar.gz &&                                                                         # unpack
+    tar xzf spotifyd-linux-armv6*.tar.gz &&                                                                         # unpack
     sudo cp ./spotifyd /usr/bin/spotifyd                                                                 # add to bin
 
 echo "Creating launch service..."
-curl "https://raw.githubusercontent.com/Spotifyd/spotifyd/master/contrib/spotifyd.service"
->/etc/systemd/user/spotifyd.service
+# create launch service file
+sudo touch /etc/systemd/user/spotifyd.service 
+curl "https://raw.githubusercontent.com/Spotifyd/spotifyd/master/contrib/spotifyd.service" | sudo tee /etc/systemd/user/spotifyd.service 
+
+# enable launch service at launch
+sudo loginctl enable-linger $(whoami)
+systemctl --user enable spotifyd.service
+
 
 echo "Creating configuration file..."
+# create config file
+CONFIG_FILE=~/.config/spotifyd/spotifyd.conf
+mkdir -p ~/.config/spotifyd/
+touch $CONFIG_FILE
+
+# write to the file
 printf "[global]\n" >$CONFIG_FILE
 printf "username = \"$SPOTIFY_USERNAME\"\n" >>$CONFIG_FILE # spotify username
 printf "password = \"$SPOTIFY_PASSWORD\"\n" >>$CONFIG_FILE # spotify password
 printf "device_name = \"$SPEAKER_NAME\"\n" >>$CONFIG_FILE  # name shown in spotify connect menu
 
+echo "Cleaning up..."
+rm spotifyd-linux-armv6*
+rm spotifyd
+
 echo "Starting spotifyd..."
-spotifyd
+systemctl --user start spotifyd.service
 
 echo "done!"
